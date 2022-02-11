53f3846047096281afb8cc5738e36ed1ed6c92a7
Zach Kangas <zkangas6@gmail.com>
2022-02-11 11:02:29 -0600

    Rules MOAR

--- handlers/rules.py
+++ handlers/rules.py
@@ -33,17 +33,19 @@ class Rules(commands.Cog, name='Handlers'):
         gid = str(payload.guild_id)
         if gid in self.rules_dict:
             guild_dict = self.rules_dict[gid]
-            if "role" not in guild_dict:
+            if str(payload.channel_id) not in guild_dict["listeners"]:
                 return
-            if payload.message_id == guild_dict["message"]:
-                guild_dict = self.rules_dict[gid]
-                channel = self.bot.get_channel(payload.channel_id)
-                message = await channel.fetch_message(payload.message_id)
-                reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
-                if reaction.emoji == guild_dict["emoji"]:
+            if str(payload.message_id) not in guild_dict["listeners"][str(payload.channel_id)]:
+                return
+            guild_dict = self.rules_dict[gid]
+            channel = self.bot.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+            for k in guild_dict["rules"]:
+                if reaction.emoji in guild_dict["rules"][k]["react"]:
                     guild = self.bot.get_guild(payload.guild_id)
                     member = guild.get_member(payload.user_id)
-                    roles = discord.utils.get(guild.roles, id=guild_dict["role"])
+                    roles = discord.utils.get(guild.roles, id=int(guild_dict["rules"][k]["react"][reaction.emoji]))
                     await member.add_roles(roles)
 
     @commands.Cog.listener()
@@ -53,75 +55,94 @@ class Rules(commands.Cog, name='Handlers'):
         gid = str(payload.guild_id)
         if gid in self.rules_dict:
             guild_dict = self.rules_dict[gid]
-            if "role" not in guild_dict:
+            if str(payload.channel_id) not in guild_dict["listeners"]:
                 return
-            if payload.message_id == guild_dict["message"]:
-                channel = self.bot.get_channel(payload.channel_id)
-                message = await channel.fetch_message(payload.message_id)
-                reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
-
-                if reaction.emoji == guild_dict["emoji"]:
+            if str(payload.message_id) not in guild_dict["listeners"][str(payload.channel_id)]:
+                return
+            guild_dict = self.rules_dict[gid]
+            channel = self.bot.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+            for k in guild_dict["rules"]:
+                if reaction.emoji in guild_dict["rules"][k]["react"]:
                     guild = self.bot.get_guild(payload.guild_id)
                     member = guild.get_member(payload.user_id)
-                    roles = discord.utils.get(guild.roles, id=guild_dict["role"])
+                    roles = discord.utils.get(guild.roles, id=int(guild_dict["rules"][k]["react"][reaction.emoji]))
                     await member.remove_roles(roles)
 
     @commands.is_owner()
     @commands.command(name='rules', aliases=['Rules', 'RULES'], hidden=True)
     async def rules(self, ctx: discord.ext.commands.Context, *arg):
-        await ctx.channel.send("owner only ovo")
+        #await ctx.channel.send("owner only ovo")
         guild_dict: dict
         if str(ctx.guild.id) not in self.rules_dict:
-            self.rules_dict[str(ctx.guild.id)] = {}
+            self.rules_dict[str(ctx.guild.id)] = {
+                "listeners": {},
+                "rules": {}
+            }
         guild_dict = self.rules_dict[str(ctx.guild.id)]
-        if arg[0] == "post":
-            name: str = arg[1]
-            guild: discord.Guild = ctx.guild
-            channel: discord.TextChannel = ctx.channel
-            embed = discord.Embed.from_dict(guild_dict[name]["embed"])
-            message: discord.Message = await channel.send(embed=embed)
-            # TODO manage reactions
-            guild_dict[name]["cid"] = message.channel.id
-            guild_dict[name]["mid"] = message.id
-            await ctx.message.delete()
-        elif arg[0] == "create":
-            name: str = arg[1]
+        rule_dict: dict
+        if len(arg) > 1 and arg[1] != "create":
+            try:
+                rule_dict = guild_dict["rules"][arg[0]]
+            except KeyError:
+                await ctx.channel.send("Call create on this name.")
+                return
+            if arg[1] == "post":
+                name: str = arg[1]
+                guild: discord.Guild = ctx.guild
+                channel: discord.TextChannel = ctx.channel
+                embed = discord.Embed.from_dict(rule_dict["embed"])
+                message: discord.Message = await channel.send(embed=embed)
+                # TODO manage reactions
+                rule_dict["cid"] = message.channel.id
+                rule_dict["mid"] = message.id
+                if "react" in rule_dict:
+                    guild_dict["listeners"][str(rule_dict["cid"])] = str(message.id)
+                    for k in rule_dict["react"]:
+                        await message.add_reaction(emoji=k)
+                await ctx.message.delete()
+            elif arg[1] == "field":
+                name = arg[1]
+                fields: list = rule_dict["embed"]["fields"]
+                fields.append({"inline": False, "name": arg[2], "value": arg[3]})
+            elif arg[1] == "clear":
+                name = arg[1]
+                rule_dict["embed"]["fields"] = []
+                rule_dict["embed"].pop("footer", None)
+            elif arg[1] == "footer":
+                name = arg[1]
+                rule_dict["embed"]["footer"] = {"text": arg[2]}
+            elif arg[1] == "preview":
+                name = arg[1]
+                try:
+                    embed = discord.Embed.from_dict(rule_dict["embed"])
+                    await ctx.channel.send(embed=embed)
+                except KeyError:
+                    await ctx.channel.send("Something went wrong")
+            elif arg[1] == "react":
+                name = arg[1]
+                if arg[2] == "remove":
+                    rule_dict.pop("react", None)
+                else:
+                    if "react" not in rule_dict:
+                        rule_dict["react"] = {}
+                    rule_dict["react"][arg[2]] = arg[3]
+        elif arg[1] == "create":
+            name: str = arg[0]
             title: str = arg[2]
             color = int(arg[3])
-            guild_dict[name] = {
+            guild_dict["rules"][arg[0]] = {
                 "embed": {"title": title, "color": color, "type": "rich", "fields": []}
             }
             await ctx.channel.send(f"Now, type %rules field/footer {name} [args]")
-        elif arg[0] == "field":
-            name = arg[1]
-            fields: list = guild_dict[name]["embed"]["fields"]
-            fields.append({"inline": False, "name": arg[2], "value": arg[3]})
-        elif arg[0] == "clear":
-            name = arg[1]
-            guild_dict[name]["embed"]["fields"] = []
-            guild_dict[name]["embed"].pop("footer", None)
-        elif arg[0] == "footer":
-            name = arg[1]
-            guild_dict[name]["embed"]["footer"] = {"text": arg[2]}
-        elif arg[0] == "preview":
-            name = arg[1]
-            try:
-                embed = discord.Embed.from_dict(guild_dict[name]["embed"])
-                await ctx.channel.send(embed=embed)
-            except KeyError:
-                await ctx.channel.send("Something went wrong")
         elif arg[0] == "list":
-            pass
+            pot = ""
+            for k in guild_dict["rules"]:
+                pot = pot + f"\t{k}\n"
+            await ctx.channel.send(f"List for server {ctx.guild.name}\n{pot}")
         elif arg[0] == "dev":
             await ctx.channel.send(f"```json\n{guild_dict}\n```")
-        elif arg[0] == "react":
-            name = arg[1]
-            if arg[2] == "remove":
-                guild_dict[name].pop("react", None)
-            else:
-                if "react" not in guild_dict[name]:
-                    guild_dict[name]["react"] = {}
-                guild_dict[name]["react"][arg[2]] = arg[3]
         with open("rules.toml", 'w', encoding="utf-8") as json_file:
             toml.dump(self.rules_dict, json_file)
         #print(self.rules_dict[str(ctx.guild.id)])
